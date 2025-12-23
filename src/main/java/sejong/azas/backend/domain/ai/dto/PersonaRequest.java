49b55980495e8cd82c4fea033fbbf0065e3a3493
ihtblwFIZ <vthyrachel@naver.com>
2025-12-23 18:23:13 +0900

    fix: dto

--- src/main/java/sejong/azas/backend/domain/ai/dto/PersonaRequest.java
+++ src/main/java/sejong/azas/backend/domain/ai/dto/PersonaRequest.java
@@ -1,9 +1,5 @@
 package sejong.azas.backend.domain.ai.dto;
 
-import com.fasterxml.jackson.databind.PropertyNamingStrategies;
-import com.fasterxml.jackson.databind.annotation.JsonNaming;
-
-@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public record PersonaRequest(
 	Float chronotype,
 	Float noiseSensitivity,
